a7d8e42c6096d3aa9e36e48dc48e881a68ce6890
henley-regatta <42374576+henley-regatta@users.noreply.github.com>
2020-12-02 11:27:48 +0000

    I mean I ought to be embarrassed about this code but... it works.

---
+++ day2part1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# My working for day 1 part 2
+#
+# PROBLEM: Given a list of <policy> : <password> tuples work out how many
+#          passwords in a list are valid/invalid
+#          <policy> looks like <num>-<num> <char>
+#          where <num>-<num> defines the number of times that <char> must appear
+#          in the <password> that follows.
+import sys
+
+pwdlist = []
+compliant = 0
+noncompliant = 0
+with open('day2_input.txt','r') as pfile:
+    for l in pfile:
+        parts = l.split(': ')
+        passwd = parts[1]
+        pol = parts[0].split()
+        pchar = pol[1]
+        range = pol[0].split('-')
+        min=int(range[0])
+        max=int(range[1])
+        numOccurrences=passwd.count(pchar)
+        pwdlist.append({'min' : min, 'max' : max, 'pchar' : pchar, 'passwd' : 'passwd', 'occurrences': numOccurrences})
+        if numOccurrences >= min and numOccurrences <= max :
+            compliant = compliant + 1
+        else :
+            noncompliant = noncompliant + 1
+
+print("Read {0} passwords. {1} complied with policy ({2} didn't)".format(len(pwdlist),compliant,noncompliant))
