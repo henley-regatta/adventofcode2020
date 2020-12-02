3b59708c956f3b85b4fd205c1aee9c98a292ce7c
henley-regatta <42374576+henley-regatta@users.noreply.github.com>
2020-12-02 10:53:47 +0000

    simple answer to day 1 part 1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # adventofcode2020
+
+I don't think I've got the chops for this one for the full list but hey ho.
+
+'day1part1.py' - brute force python approach to the solution. Works.
--- day1part1.py
+++ day1part1.py
@@ -2,7 +2,7 @@
 # My working for day 1 part 1
 #
 # PROBLEM: Given a list of input numbers, work out which pair sum to 2020
-#          Calculate the sum of that pair as the result.
+#          Calculate the product of that pair as the result.
 import sys
 
 #Get the input list
@@ -12,3 +12,11 @@ with open("day1_input.txt",'r') as numfile:
         numlist.append(int(l))
 
 print("Read {0} entries from numberlist".format(len(numlist)))
+
+#Brute force the list to find the sums.
+for i in range(len(numlist)) :
+    x = numlist[i]
+    for j in range(len(numlist)) :
+        if i != j : #don't compare same number twice
+            if x + numlist[j] == 2020:
+                print("elem {0} - {1} and {2} - {3} sum to 2020 with product {4}".format(i,x,j,numlist[j], x*numlist[j]) )
